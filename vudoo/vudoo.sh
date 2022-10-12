56d24f0a17c5cb0698f21593c96de40fa1ade9d4
Rictoshi <rick_navarro@protonmail.ch>
2022-10-11 22:51:35 -0500

    1.- Removed the touch and mv commands along with the creation of     temp file for more eficient single line "cat $header > $1.sh"     at line 41.
    2.- Added Check for null argumet in line 32.
    
    3.- Added chmod set from script argument by user.

--- vudoo/vudoo.sh
+++ vudoo/vudoo.sh
@@ -1,31 +1,54 @@
 #!/bin/bash
 
-######################################################################
-# *       Filename: vudoo.sh  
+######################################################################################
+# *       Filename: vudu.sh  
 # *
 # *    Description: Shell script written to ease the creation of a shell
 # *                 script.
 # *
-# *        Version:  1.0
+# *        Version:  2.0
 # *        Created:  26/04/2012
-# *       Revision:  none
+# *		
+# *       Revision:  14/01/2013
+# *		    
+# *		     1.- Removed the touch and mv commands along with the creation of
+# * 			 temp file for more eficient single line "cat $header > $1.sh"
+# *			 at line 41. 
+# *		     
+# *		     2.- Added Check for null argumet in line 32. 
+# *		
+# *		     3.- Added chmod set from script argument by user.
 # *
 # *         Author:  Ricktoshi, enrique_nav@hotmail.com
 # *        Company:  NavCom
 # *
-#######################################################################
+#######################################################################################
 
-Header=~/Templates/headers/sh_header.txt
+Header=~/Template/headers/sh_header.txt
+
 script_name=$1
 
-#[ $# -eq "" ] && {echo "Please enter a the name of the shell script you wish to create"; exit 1}
+#The -z switch will test if the expansion of "$1" is a null string or not. If it is a
+#null string then the body is executed.
 
- touch $1.sh
- cat $Header $1.sh > $file.temp
- mv $file.temp $1.sh
+# [  -z "$1" ] && {echo echo "Please enter the name of the shell script you wish to create"; exit 1 } 
+
+if [  -z "$1" ]
+   then
+        echo "Please enter the name of the shell script you wish to create"
+        exit 1
+  else
+	cat $Header > $1.sh
+fi
 
 # add code to accept a 2nd argument and if present change chmod to user specified value
- chmod 744 $1.sh
+
+if [  -z "$2" ]
+  then 
+	chmod 744 $1.sh 
+  else
+        chmod $2 $1.sh
+fi
 
 vim $1.sh
  
