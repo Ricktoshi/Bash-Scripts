ef15ab879f209b46e4765fc131f1591024ae41f7
Rictoshi <rick_navarro@protonmail.ch>
2022-10-13 20:37:05 -0500

    Added "sh", "pl" and "htm" id for creating bash, perl and  "htm" or "html" scripts.

--- vudoo/vudoo.sh
+++ vudoo/vudoo.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 ######################################################################################
-# *       Filename: vudu.sh  
+# *       Filename: vudoo.sh 
 # *
 # *    Description: Shell script written to ease the creation of a shell
 # *                 script.
 # *
-# *        Version:  2.0
+# *        Version:  2.5
 # *        Created:  26/04/2012
 # *		
-# *       Revision:  14/01/2013
+# *       Revision:  04/02/2013
 # *		    
 # *		     1.- Removed the touch and mv commands along with the creation of
 # * 			 temp file for more eficient single line "cat $header > $1.sh"
@@ -18,14 +18,18 @@
 # *		     2.- Added Check for null argumet in line 32. 
 # *		
 # *		     3.- Added chmod set from script argument by user.
+# *		
+# *		     4.- Added "sh", "pl" and "htm" id for creating bash, perl and 
+# *			 "htm" or "html" scripts.
 # *
-# *         Author:  Ricktoshi, enrique_nav@hotmail.com
+# *         Author:  Ricktoshi, rick_navarro@protonmail.ch
 # *        Company:  NavCom
 # *
 #######################################################################################
 
-Header=~/Template/headers/sh_header.txt
-
+SH_header=~/Templates/headres/sh_header.txt
+PL_header=~/Templates/headres//pl_header.txt 
+HTML_header=~/Templates/headres/htm_header.txt
 script_name=$1
 
 #The -z switch will test if the expansion of "$1" is a null string or not. If it is a
@@ -37,20 +41,35 @@ if [  -z "$1" ]
    then
         echo "Please enter the name of the shell script you wish to create"
         exit 1
-  else
-	cat $Header > $1.sh
+#  else
+#	cat $Header > $1.sh
+fi
+
+
+if [[ "$1" == *.sh* ]]
+	then
+	cat $SH_header > $1
+
+	elif [[ "$1" == *.pl* ]]
+	then
+	cat $PL_header > $1
+
+		elif [[ "$1" == *.htm* ]]
+		then
+        	cat $HTML_header > $1
+
 fi
 
 # add code to accept a 2nd argument and if present change chmod to user specified value
 
 if [  -z "$2" ]
   then 
-	chmod 744 $1.sh 
+	chmod 744 $1 
   else
-        chmod $2 $1.sh
+        chmod $2 $1
 fi
 
-vim $1.sh
+vim $1
  
 
 
